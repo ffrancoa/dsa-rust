7c9ed0cf176a01d9e4672da90d9e9e136b474738
ffrancoa <ffrancoa97@gmail.com>
2025-07-31 02:13:44 -0500

    Improve the problem description.

--- exercises/leetcode/01_two_sum/src/lib.rs
+++ exercises/leetcode/01_two_sum/src/lib.rs
@@ -1,11 +1,13 @@
-//  Two Sum
+// Two Sum
 //
+// Description
+// -----------
 // Given an array of integers `nums` and an integer `target`, return indices of the two
 // numbers such that they add up to `target`.
 //
 // You may assume that each input would have *exactly one solution*, and you may not
 // use the same element twice.
-
+//
 // You can return the answer in any order.
 
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
