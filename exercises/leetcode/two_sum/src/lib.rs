1c2d54b4f4ac53e75c7606638484b1909cc1afd0
ffrancoa <ffrancoa97@gmail.com>
2025-08-06 21:14:17 -0500

    Improve the 'two_sum' exercise.

--- exercises/leetcode/two_sum/src/lib.rs
+++ exercises/leetcode/two_sum/src/lib.rs
@@ -1,7 +1,5 @@
 // Two Sum
 //
-// Description
-// -----------
 // Given an array of integers `nums` and an integer `target`, return indices of the two
 // numbers such that they add up to `target`.
 //
@@ -9,6 +7,13 @@
 // use the same element twice.
 //
 // You can return the answer in any order.
+//
+// Constraints
+// -----------
+// 2 ≤ `nums.length` ≤ 10^4
+// -10^9 ≤ `nums.length` ≤ 10^9
+// -10^9 ≤ `target` ≤ 10^9
+// Only one valid answer exists.
 
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     todo!("pending solution!")
